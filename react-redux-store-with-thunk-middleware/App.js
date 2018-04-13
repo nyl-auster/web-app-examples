baee14577daea258870a0efe70c60de81308e4b0
yann <yann.boisselier@gmail.com>
2018-04-13 22:50:25 +0200

    add redux example

---
+++ react-redux-store-with-thunk-middleware/App.js
@@ -0,0 +1,29 @@
+﻿import "./polyfill";
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import HomePage from "./modules/homepage/pages/HomePage";
+import SearchPage from "./modules/search/pages/SearchPage";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <MuiThemeProvider>
+            <div className="App">
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/search" component={SearchPage} />
+              </Switch>
+            </div>
+          </MuiThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
